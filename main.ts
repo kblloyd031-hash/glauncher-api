e30d8e664c17305bf87fa72f13477096dd65bc03
kblloyd031-hash <kblloyd031@gmail.com>
2026-07-12 07:40:45 +0300

    Fix formatting of maintenanceMode property

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ Deno.serve(async (req) => {
   // The main config endpoint
   if (url.pathname === "/app-config") {
     const config = {
-      maintenanceMode: true,
+      maintenanceMode:true,
       minVersion: 1,
       features: {
         enableHighQualitySniffing: true,
