70320216b00d94476d9769b51f57d1eccf801e3a
kblloyd031-hash <kblloyd031@gmail.com>
2026-07-21 19:34:04 +0300

    Update app version to 1.0.2

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ const kv = await Deno.openKv();
 
 // --- ⚙️ CONFIGURATION ---
 const ADMIN_PASSWORD = "admin"; 
-const LATEST_APP_VERSION = "1.0.0"; 
+const LATEST_APP_VERSION = "1.0.2"; 
 const MIN_APP_VERSION = "2";       
 const UPDATE_URL = "https://pub-532ae1cec0544157b46e07176dc7b9d2.r2.dev/app-debug.apk";
 
